eb4c985aced848c4087ee5f47066c9f0c2dddcae
tmali16 <don.kg16@gmail.com>
2021-01-05 00:51:00 +0600

    transe

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class="container h-100">
     <div class="row h-100 justify-content-center">
-        <div class="col-md-5 my-auto">
+        <div class="col-md-6 my-auto">
             <div class="card border-primary">
                 <div class="card-header">{{ __("Вход") }}</div>
                 <div class="card-body bg-transparent">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Имя пользователя') }}</label>
+                            <label for="username" class="col-md-4 col-form-label text-md-left">{{ __('Имя пользователя') }}</label>
 
                             <div class="col-md-6">
                                 <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
@@ -24,7 +24,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Пароль') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-left">{{ __('Пароль') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
